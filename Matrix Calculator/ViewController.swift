9299c9fcb8c225673b8bc0902e5a8e1267ce8f8e
DBL-Lee <hzc930916@live.cn>
2015-02-12 12:59:43 +0000

    Initial Commit

---
+++ Matrix Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Matrix Calculator
+//
+//  Created by Zichuan Huang on 12/02/2015.
+//  Copyright (c) 2015 Zichuan Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
