85b7e420a0346dc42310b49eccf3dffc1623c2c8
Ivy <sssssivy@gmail.com>
2015-07-20 22:31:52 +0100

    add polynomial

---
+++ Matrix Calculator/Polynomial.swift
@@ -0,0 +1,13 @@
+//
+//  Polynomial.swift
+//  MCalculator
+//
+//  Created by Ying on 15/7/2.
+//  Copyright (c) 2015年 Ying. All rights reserved.
+//
+
+import UIKit
+
+class Polynomial: NSObject {
+   
+}
